c57a62bf1477b1a06d0ca04ca72c7f69ee6545e1
rival <rivalsjules@gmail.com>
2026-05-20 18:22:12 +0200

    classes

---
+++ src/projet_java/Recette.java
@@ -0,0 +1,30 @@
+package projet_java;
+
+public class Recette {
+	int id;
+	String nom;
+	
+	public Recette(int id, String nom) {
+		this.id = id;
+		this.nom = nom;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
